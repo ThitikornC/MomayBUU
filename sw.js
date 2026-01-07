ab7dd2ba6326b076e01e5cc25efb777a51b997e2
ThitikornC <tiice01@gmail.com>
2026-01-07 11:27:46 +0700

    chore: update index.html and sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'momay-cache-vB2';
 const PRECACHE_URLS = [
   '/',
   '/index.html',
-  '/style.css?v=2.8',
-  '/script.js?v=2.8',
+  '/style.css?v=2.8.1',
+  '/script.js?v=2.8.1',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
